2ae2c669c023c0e766203d32366306b0a539044c
Arnaud AKOTONOU <Arnaud AKOTONOU@erugis.sas>
2022-12-10 14:07:21 +0100

    feat: add
    - added steward Img on MainLayout
    - add icon and user avatar on NavSection

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -45,44 +45,51 @@ export default function MainLayout() {
       <section className="w-full bg-no-repeat bg-cover" style={{ backgroundImage: `url("/assets/bg1.png")` }}>
         <div className=" bg-cover" style={{ backgroundImage: `url("/assets/bgBlur.png")` }}>
           <NavSection />
-          <div className="bg-white rounded-2xl border border-gray-100 shadow-2xl shadow-gray-200 px-12 py-8 mx-[10%] min-h-[128px] relative bottom-[-10vh] ">
-            <Tabs value={activeTab} onTabChange={setActiveTab} >
-              <Tabs.List className='w-[86%] border-b border-gray-200' >
-                <Tabs.Tab value='hotel' pb="lg" icon={<Icon icon='bed' color={activeTab === 'hotel' ? 'black' : 'gray'} height={16} />} px={0} mr="lg" > <p className={`font-bold text-xs ${activeTab === 'hotel' ? 'text-gray-800' : 'text-gray-500'}`}>Hotel</p> </Tabs.Tab>
-                <Tabs.Tab value='flight' pb="lg" icon={<Icon icon='plane' color={activeTab === 'flight' ? 'black' : 'gray'} height={22} />} px={0} mr="lg" > <p className={`font-bold text-xs ${activeTab === 'flight' ? 'text-gray-800' : 'text-gray-500'}`}>Flight</p></Tabs.Tab>
-                <Tabs.Tab value='car' pb="lg" icon={<Icon icon='taxi' color={activeTab === 'car' ? 'black' : 'gray'} height={18} />} px={0} mr="lg" > <p className={`font-bold text-xs ${activeTab === 'car' ? 'text-gray-800' : 'text-gray-500'}`}>Car Rental</p> </Tabs.Tab>
-                <div className="flex items-center ml-auto gap-x-4">
-                  <span className='flex items-center gap-x-3'>
-                    <p className='text-gray-600 font-medium'>Round trip</p>
-                    <IconChevronDown color='gray' size={16} />
-                  </span>
+          <div className="grid grid-cols-12 relative bottom-[-10vh]">
+            <div className="col-span-1"></div>
+            <div className="col-span-10 bg-white rounded-2xl border border-gray-100 shadow-2xl shadow-gray-200 px-12 py-8 min-h-[128px]  ">
+              <Tabs value={activeTab} onTabChange={setActiveTab} >
+                <Tabs.List className='w-[86%] border-b border-gray-200' >
+                  <Tabs.Tab value='hotel' pb="lg" icon={<Icon icon='bed' color={activeTab === 'hotel' ? 'black' : 'gray'} height={16} />} px={0} mr="lg" > <p className={`font-bold text-xs ${activeTab === 'hotel' ? 'text-gray-800' : 'text-gray-500'}`}>Hotel</p> </Tabs.Tab>
+                  <Tabs.Tab value='flight' pb="lg" icon={<Icon icon='plane' color={activeTab === 'flight' ? 'black' : 'gray'} height={22} />} px={0} mr="lg" > <p className={`font-bold text-xs ${activeTab === 'flight' ? 'text-gray-800' : 'text-gray-500'}`}>Flight</p></Tabs.Tab>
+                  <Tabs.Tab value='car' pb="lg" icon={<Icon icon='taxi' color={activeTab === 'car' ? 'black' : 'gray'} height={18} />} px={0} mr="lg" > <p className={`font-bold text-xs ${activeTab === 'car' ? 'text-gray-800' : 'text-gray-500'}`}>Car Rental</p> </Tabs.Tab>
+                  <div className="hidden lg:flex items-center ml-auto gap-x-4">
+                    <span className='flex items-center gap-x-3'>
+                      <p className='text-gray-600 font-medium'>Round trip</p>
+                      <IconChevronDown color='gray' size={16} />
+                    </span>
 
-                  <PassengerDropdown
-                    buttonComponent={
-                      <span className='cursor-pointer flex items-center  gap-x-3'>
-                        <p className='text-gray-600 !font-medium'>1 passenger</p>
-                        <IconChevronDown color='gray' size={16} />
-                      </span>
-                    }
-                    items={[{ label: 'Adults', caption: 'Ages 13 or above' }, { label: 'Adults', caption: 'Ages 2-12' }, { label: 'Infants', caption: 'under 2' }]}
-                  />
-                </div>
-              </Tabs.List>
+                    <PassengerDropdown
+                      buttonComponent={
+                        <span className='cursor-pointer flex items-center  gap-x-3'>
+                          <p className='text-gray-600 !font-medium'>1 passenger</p>
+                          <IconChevronDown color='gray' size={16} />
+                        </span>
+                      }
+                      items={[{ label: 'Adults', caption: 'Ages 13 or above' }, { label: 'Adults', caption: 'Ages 2-12' }, { label: 'Infants', caption: 'under 2' }]}
+                    />
+                  </div>
+                </Tabs.List>
 
-              <Tabs.Panel value='hotel' pt="xl">
-                {getTabForm('hotel')}
-              </Tabs.Panel>
+                <Tabs.Panel value='hotel' pt="xl">
+                  {getTabForm('hotel')}
+                </Tabs.Panel>
 
-              <Tabs.Panel value='flight' pt="xl">
-                {getTabForm('flight')}
-              </Tabs.Panel>
+                <Tabs.Panel value='flight' pt="xl">
+                  {getTabForm('flight')}
+                </Tabs.Panel>
 
-              <Tabs.Panel value='car' pt="xl">
-                {getTabForm('car')}
-              </Tabs.Panel>
-            </Tabs>
+                <Tabs.Panel value='car' pt="xl">
+                  {getTabForm('car')}
+                </Tabs.Panel>
+              </Tabs>
 
+            </div>
+            <div className="col-span-1">
+              <img src='/assets/steward.png' className='hidden lg:block h-[550px] absolute right-10 bottom-5' />
+            </div>
           </div>
+
         </div>
       </section>
       <section className='container px-[10%]'>
--- src/components/NavSection.tsx
+++ src/components/NavSection.tsx
@@ -20,15 +20,16 @@ export default function NavSection({ withIconImage = true }: { withIconImage?: b
           <h2 className="text-4xl lg:text-5xl font-bold text-blue-700 ">Teska corp.</h2>
         </div>
       }
-      <nav className="mt-1 h-10 ml-auto flex items-center justify-end gap-x-2">
+      <nav className="mt-1 h-10 ml-auto flex items-center justify-end gap-x-3">
         <span className="text-sm font-medium text-gray-500">USD</span>
-        <span className="text-red-500">flag</span>
+        <img className='w-6 h-6' src="/assets/language.svg" alt="" />
         <Indicator className="mr-5" radius="md" size={16} label={3} offset={7} color="red" >
           <ActionIcon variant="transparent"><IconBell size={32} /></ActionIcon>
         </Indicator>
         <div className="flex items-center gap-x-2">
-          <span className="w-10 h-10 rounded-full bg-gray-500 border border-gray-50"></span>
-          <p className="text-sm font-medium text-gray-500">Delowar</p>
+          {/* <span className="w-10 h-10 rounded-full bg-gray-500 border border-gray-50"></span> */}
+          <img className="w-8 border border-gray-50" src="/assets/user.png" alt="" />
+          <p className="hidden lg:block text-sm font-medium text-gray-500">Delowar</p>
         </div>
       </nav>
     </div>
